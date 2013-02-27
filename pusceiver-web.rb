bf180e9a99e6a7ef5f3b0304e53c129cc8f8fb13
Hiroshi Saito <hiroshi3110@gmail.com>
2013-02-27 16:31:45 +0900

    Implemented push form

--- pusceiver-web.rb
+++ pusceiver-web.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 
 #set :public_folder, "."
 
-get '/login' do
-  erb :login, :locals => {:firebase_url => (ENV['FIREBASE_URL'] || 'https://pusceiver.firebaseio.com/')}
+locals = {:firebase_url => (ENV['FIREBASE_URL'] || 'https://pusceiver.firebaseio.com/')}
+
+get '/' do
+  erb :index, :locals => locals
 end
+
+get '/login' do
+  erb :login, :locals => locals
+end
+
